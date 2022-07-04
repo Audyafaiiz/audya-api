ada004add7430fca168e1219198ce1da609d6630
Audyafaiiz <audyafaiz@gmaol.com>
2022-07-04 13:13:44 +0700

    upload

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,6 +2,6 @@ module.exports = {
     multipleStatements: true,
     host: 'db4free.net',
     user: 'audya1',
-    password: 'BANDUNG123',
+    password: 'bandung123',
     database: 'dbsiswa'
 };
